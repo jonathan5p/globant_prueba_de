2b8c1543b32081462503573dcc787583a33e5e59
jonatsrp <jonatsrp@amazon.com>
2023-05-07 17:51:16 -0500

    addition of required query paraemters check in db_migration lamdba

--- lambda/db_migration/db_migration.py
+++ lambda/db_migration/db_migration.py
@@ -7,6 +7,31 @@ import pandas as pd
 secret_name = os.environ["secret_name"]
 
 
+def _check_input_parameters(query_parameters: dict):
+    res = None
+
+    if query_parameters == None:
+        res = {
+            "statusCode": 400,
+            "body": "Table and schema are required parameters",
+            "headers": {"Content-Type": "*/*"},
+        }
+    elif "table" not in query_parameters.keys():
+        res = {
+            "statusCode": 400,
+            "body": "Table is a required parameter",
+            "headers": {"Content-Type": "*/*"},
+        }
+    elif "schema" not in query_parameters.keys():
+        res = {
+            "statusCode": 400,
+            "body": "Schema is a required parameter",
+            "headers": {"Content-Type": "*/*"},
+        }
+
+    return res
+
+
 def _read_csv(payload_body: str):
     csv_data = StringIO(payload_body)
     df = pd.read_csv(csv_data, sep=",", engine="pyarrow")
@@ -15,18 +40,23 @@ def _read_csv(payload_body: str):
 
 def _send_data_to_mysql(data_df: pd.DataFrame, table: str, schema: str):
     con = wr.mysql.connect(secret_id=secret_name)
-    tables = wr.mysql.read_sql_query(sql="show tables;",con=con)
-    if table in tables.iloc[:,0].to_list():
+    tables = wr.mysql.read_sql_query(sql="show tables;", con=con)
+    if table in tables.iloc[:, 0].to_list():
         with con.cursor() as cursor:
             cursor.execute(f"DROP TABLE {table}")
     wr.mysql.to_sql(df=data_df, table=table, schema=schema, con=con)
     con.close()
-    
+
 
 def lambda_handler(event, context):
     print("Received event:", json.dumps(event))
 
-    if event["headers"].get("Content-Type") == "text/csv":
+    res = {"statusCode": 200, "headers": {"Content-Type": "*/*"}}
+    res["body"] = f"Data loaded succesfully!"
+
+    chk_params = _check_input_parameters(event.get("queryStringParameters"))
+
+    if event["headers"].get("Content-Type") == "text/csv" and chk_params == None:
         df = _read_csv(event.get("body"))
         table = event.get("queryStringParameters").get("table")
         schema = event.get("queryStringParameters").get("schema")
@@ -36,7 +66,7 @@ def lambda_handler(event, context):
 
         _send_data_to_mysql(data_df=df, table=table, schema=schema)
 
-    res = {"statusCode": 200, "headers": {"Content-Type": "*/*"}}
+    elif chk_params != None:
+        res = chk_params
 
-    res["body"] = f"Data loaded succesfully!"
     return res
